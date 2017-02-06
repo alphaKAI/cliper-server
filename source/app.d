371f14188c8d819b6da7b73fe3183d98b0ca4b3a
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2017-02-07 00:05:06 +0900

    make PORT configuratable

--- source/app.d
+++ source/app.d
@@ -1,25 +1,36 @@
 module app;
-import std.process,
-       std.stdio;
-import vibe.d;
-import mongoschema;
 import cliperserver.schema.v1.buffer,
        cliperserver.schema.v1.user;
 import cliperserver.api.v1.buffer,
        cliperserver.api.v1.user;
+import std.process,
+       std.string,
+       std.stdio,
+       std.conv;
+import mongoschema;
+import vibe.d;
 
 enum string LATEST_GIT_HASH = import("./version");
-import std.string;
 private void api_v1_get_version(HTTPServerRequest req, HTTPServerResponse res) {
   res.writeBody(`{"success":"running", "version":"` ~ LATEST_GIT_HASH.chomp ~`"}`, "application/json");
 }
 
+enum DEFAULT_PORT = 3017;
+
 shared static this () {
+
+  immutable string CLIPER_SERVER_PORT      = environment.get("CLIPER_SERVER_PORT");
+  immutable string CLIPER_SERVER_MONGO_DSN = environment.get("CLIPER_SERVER_MONGO_DSN");
+
   auto settings = new HTTPServerSettings;
-  settings.port = 3017;
   settings.accessLogToConsole = true;
 
-  immutable string CLIPER_SERVER_MONGO_DSN = environment.get("CLIPER_SERVER_MONGO_DSN");
+  if (CLIPER_SERVER_PORT is null) {
+    settings.port = DEFAULT_PORT;
+  } else {
+    settings.port = CLIPER_SERVER_PORT.to!ushort;
+  }
+
   if (CLIPER_SERVER_MONGO_DSN is null) {
     throw new Error("Please set $CLIPER_SERVER_MONGO_DSN in environment variable.");
   }
@@ -28,6 +39,7 @@ shared static this () {
 
   client.getCollection("cliperserver.users").register!User;
   client.getCollection("cliperserver.buffers").register!Buffer;
+
   auto router = new URLRouter;
 
   router.get("/api/v1/version", &api_v1_get_version);
